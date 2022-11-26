f564129a405eaae3b99a9d75b095792f9f9d0d95
Marcela Bustamante <julbustamante@uade.edu.ar>
2022-11-26 14:28:22 -0300

    fix logout

--- app/networking/api/Api.js
+++ app/networking/api/Api.js
@@ -1,26 +1,6 @@
 import axios from 'axios';
 import { config } from '../../config/config';
-//import { store } from '../../redux/Store';
 
-axios.defaults.baseURL = config.BASE_URL;
-axios.defaults.timeout = config.TIME_OUT;
-axios.defaults.headers.common = {
-Accept: 'application/json' , // el formato que espero que la info vuelva
-'Content-Type' : 'application/json' ,// el formato en que le mando la info
-};
-
-axios.interceptors.response.use(
-  function (response) {
-    console.log(response);
-    return response;
-  },
-  function (error) {
-    console.error(error);
-    return Promise.reject(error);
-  }
-);
-
-// TODO
 const instance = axios.create({
   baseURL: config.BASE_URL,
   timeout: config.TIME_OUT,
@@ -32,25 +12,16 @@ const instance = axios.create({
   }
 });
 
-
 export function setAuthToken(token) {
-    axios.defaults.headers.common['Authorization'] = '';
-    delete axios.defaults.headers.common['Authorization'];
-
     instance.defaults.headers.common['Authorization'] = '';
     delete instance.defaults.headers.common['Authorization'];
-
     if (token) {
       console.log("Setting token " + token);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
       instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     } else {
         console.log("No token privided");
     }
   }
-
-// export default axios;
-
 
 instance.interceptors.response.use(
   function (response) {
