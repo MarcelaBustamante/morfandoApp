47494b0e53d397d474401a10236dea60b30bef3a
xMavenCalore <90734663+xMavenCalore@users.noreply.github.com>
2022-12-04 22:28:45 -0300

    empieza la aventura del editar

--- app/ui/screens/restaurante/EditRestaurantScreen1.js
+++ app/ui/screens/restaurante/EditRestaurantScreen1.js
@@ -1,19 +1,124 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import EditRestaurantScreen1UI from './EditRestaurantScreen1UI';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import NavigatorConstant from '../../../navigation/NavigatorConstant';
+import { useFormik } from 'formik';
+import { initialValues, validationSchemma } from './RestaurantScreen.data';
+import { useEffect } from "react";
+import { getRestoAPI } from "../../../networking/api/endpoints/restaurantsWS";
+import { useDispatch, useSelector } from 'react-redux';
+import { updateRestaurant } from "../../../redux/slices/newRestaurantsSlice";
+import { InitialDaysScreen } from './DaysScreen.data';
+import Toast from 'react-native-simple-toast';
+import moment from 'moment';
 
-export default function EditRestaurantScreen1({navigation}) {
+export default function EditRestaurantScreen1({navigation, route}) {
+  const {restaurant}  = route.params;
+  const dispatch = useDispatch();
+  const dataPrice = [
+    { label: '$', value: '1' },
+    { label: '$$', value: '2' },
+    { label: '$$$', value: '3' },
+  ];
+  const restaurantType = [
+    { label: 'ALEMANA', value: '1' },
+    { label: 'ARGENTINA', value: '2' },
+    { label: 'ARMENIA', value: '3' },
+    { label: 'CHINA', value: '4' },
+    { label: 'INDIA', value: '5' },
+    { label: 'JAPONESA', value: '6' },
+    { label: 'NORTEAMERICANA', value: '7' },
+    { label: 'PERUANA', value: '8' },
+  ];
+
+  const [businessHours, setBusinessHours] = useState(
+    InitialDaysScreen
+  );
+  const [selected, setSelected] = useState([]); //dropdown menu tags
+  const [valuePriece, setValuePriece] = useState(1);
+  const { isLoading, status } = useSelector(state => state.restaurant);
+  const [checked, setChecked] = useState(false);
+  const [name, setName] = useState(restaurant.name);
+  const [street, setStreet] = useState(restaurant.address.street);
+  const [number, setNumber] = useState(restaurant.address.number);
+  const [pictures, setPictures] = useState([]);
+
   welcomeString = 'Bienvenido a Morfando';
 
-  const loginHandler = () => {
-    console.log("Hola mundo");
-  }
+  const formik = useFormik({
+     initialValues: initialValues(),
+     validateOnChange: false,
+     onSubmit: async (formValue) =>{
+      dispatch(updateRestaurant({ formValue, restoId: restaurant.id }));
+      console.log("el formulario: ", formValue)
+       navigation.navigate(NavigatorConstant.LANDING_STACK.RESTAURANT_DETAILS, { restaurant });
+     }
+   });
+ 
+
+  const changeSingleBusinessHour = (dayOfWeek, attr, newValue) => {
+    return setBusinessHours(businessHours => {
+      return businessHours.map(businessHour => {
+        if (businessHour.dayOfWeek === dayOfWeek) {
+          return { ...businessHour, [attr]: newValue };
+        }
+        return businessHour;
+      });
+    });
+  };
+  const onRestaurantSubmit = (e) => {
+    e.preventDefault();
+    const hours = businessHours
+      .filter(day => day.active === true)
+      .map((day) => {
+        return {
+          dayOfWeek: day.dayOfWeek,
+          fromTime: moment(day.fromTime).format('HH:mm:ss'),
+          toTime: moment(day.toTime).format('HH:mm:ss')
+        }
+      });
+    dispatch(updateRestaurant({
+      "address": {
+        "street": street,
+        "number": number,
+      },
+      "businessHours": hours,
+      "name": name,
+      "type": selected.label,
+      "priceRange": valuePriece,
+      "photos": pictures,
+      "active": !checked
+    }));
+  };
+
+
+
+
   return (
     <KeyboardAwareScrollView>
           <EditRestaurantScreen1UI
-            navigateToMenuOwner={() => navigation.navigate(NavigatorConstant.LANDING_STACK.RESTAURANT_DETAILS, {restaurant})}
-            navigateToEditScreen2={() => navigation.navigate(NavigatorConstant.EDIT_RESTAURANT_STACK.EDIT_SCREEN_2)}
-            loginHandler={loginHandler}/> 
+            dataPrice={dataPrice}
+            dataTypeFood={restaurantType}
+            businessHours={businessHours}
+            selected={selected}
+            setSelected={setSelected}
+            valuePriece={valuePriece}
+            setValuePriece={setValuePriece}
+            checked={checked}
+            setChecked={setChecked}
+            changeSingleBusinessHour={changeSingleBusinessHour}
+            onRestaurantSubmit={onRestaurantSubmit}
+            isLoading={isLoading}
+            formik= {formik}
+            restaurant={restaurant}
+            name={name}
+            street={street}
+            number={number}
+            pictures={pictures}
+            setName={setName}
+            setStreet={setStreet}
+            setNumber={setNumber}
+            setPictures={setPictures}
+            /> 
     </KeyboardAwareScrollView>
   )};
--- app/ui/screens/restaurante/EditRestaurantScreen1UI.js
+++ app/ui/screens/restaurante/EditRestaurantScreen1UI.js
@@ -1,216 +1,325 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity} from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
 import Theme from '../../styles/Theme';
-import { Button  } from "@react-native-material/core";
+import { Button ,  ListItem, Switch } from "@react-native-material/core";
+import { Formik } from 'formik';
+import FileUploadButton from '../../components/shared/FileUploadButton';
+import { Input } from '@rneui/themed';
+import { MapForm } from '../../components/MapsForm/MapsForm';
+import { LoadingModal } from '../../components/shared/LoadingModal/LoadingModal';
+import Avatar from '../../components/shared/AvatarCustom';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
+import { filter } from 'lodash';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import CheckBox from '@react-native-community/checkbox';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
-const dataCountry = [
-  { label: 'Argetina', value: '1' },
-];
-const dataProvince = [
-  { label: 'Buenos Aires', value: '2' },
-  { label: 'Mendoza', value: '3' },
-  { label: 'Santa Fé', value: '4' },
-  { label: 'San Juan', value: '5' },
-  { label: 'Santiago del Estero', value: '6' },
-];
-const dataLocation = [
-  { label: 'Quilmes', value: '7' },
-  { label: 'Florencio Varela', value: '8' },
-  { label: 'San Isidro', value: '9' },
-  { label: 'Capital Federal', value: '10' },
-  { label: 'Lanús', value: '11' },
-];
-const dataNeighborhood = [
-  { label: 'Barrio de la localidad seleccionada', value: '12' },
-];
 
 const EditRestaurantScreen1UI = ({
   primText = 'No vino ningún texto',
   secText,
   loginHandler,
-  navigateToMenuOwner,
-  navigateToEditScreen2,
+  formik,
+  restaurant,
+  dataTypeFood,
+  dataPrice,
+  selected,
+  setSelected,
+  valuePriece,
+  setValuePriece,
+  businessHours,
+  checked,
+  setChecked,
+  changeSingleBusinessHour,
+  onRestaurantSubmit,
+  isLoading,
+  name,
+  street,
+  number,
+  pictures,
+  setName,
+  setStreet,
+  setNumber,
+  setPictures,
 }) => {
-  const [valueCoutry, setValueCoutry] = useState(null);
-  const [valueLocation, setValueLocation] = useState(null);
-  const [valueProvince, setValueProvince] = useState(null);
-  const [valueNeighborhood, setValueNeighborhood] = useState(null);
+
+  
+  console.log("photos: ",restaurant.photos)
+  const [showMap, setShowMap] = useState(false);
 
   const [isFocus, setIsFocus] = useState(false);
 
+  console.log(restaurant)
 
-  const renderLabelCountry = () => {
-    if (valueCoutry || isFocus) {
+  const [value, setValue] = useState(null);
+
+
+  const renderItem = (item) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.selectedTextStyle}>{item.label}</Text>
+        <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+      </View>
+    );
+  };
+  const renderLabelPrice = () => {
+    if (value || isFocus) {
       return (
         <Text style={[styles.label, isFocus && { color: Theme.colors.PRIMARY }]}>
-          País
+          Precio
         </Text>
       );
     }
     return null;
   };
-  const renderLabelProvince = () => {
-    if (valueProvince || isFocus) {
-      return (
-        <Text style={[styles.label, isFocus && { color: Theme.colors.PRIMARY }]}>
-          Provincia
-        </Text>
-      );
-    }
-    return null;
+
+  const renderBusinessHour = (dayState, i) => {
+    return (
+      <View key={dayState.label}> 
+        <View  key={i} style={{ flexDirection: "row" }}>
+          <Text style={styles.days}>{dayState.label}</Text>
+          <CheckBox
+            disabled={false}
+            value={dayState.active}
+            onValueChange={(newValue) => changeSingleBusinessHour(dayState.dayOfWeek, "active", newValue)}
+          />
+        </View>
+        <View style={{ flexDirection: "row", justifyContent: "center", marginBottom: 8 }} >
+          <View style={styles.MainContainer}>
+            {!dayState.showFrom && (
+              <View>
+                <Button title="De:" color={Theme.colors.SECONDARY} marginHorizontal={2}
+                  onPress={() => changeSingleBusinessHour(dayState.dayOfWeek, "showFrom", true)}
+                />
+              </View>
+            )}
+
+            <Text style={styles.text}>{dayState.fromTime?.toLocaleTimeString('en-US')}</Text>
+            {dayState.showFrom && (
+              <DateTimePicker
+                value={dayState.fromTime}
+                mode={'time'}
+                is24Hour={false}
+                onChange={(event, value) => {
+                  changeSingleBusinessHour(dayState.dayOfWeek, "showFrom", false);
+                  changeSingleBusinessHour(dayState.dayOfWeek, "fromTime", value);
+                }}
+                style={styles.datePicker}
+              />
+            )}
+          </View>
+          <View style={styles.MainContainer}>
+            {!dayState.showTo && (
+              <View>
+                <Button title="Hasta:" color={Theme.colors.SECONDARY} marginHorizontal={2}
+                  onPress={() => changeSingleBusinessHour(dayState.dayOfWeek, "showTo", true)}
+                />
+              </View>
+            )}
+            <Text style={styles.text}>{dayState.toTime?.toLocaleTimeString('en-US')}</Text>
+            {dayState.showTo && (
+              <DateTimePicker
+                value={dayState?.toTime}
+                mode={'time'}
+                is24Hour={false}
+                onChange={(event, value) => {
+                  changeSingleBusinessHour(dayState.dayOfWeek, "showTo", false);
+                  changeSingleBusinessHour(dayState.dayOfWeek, "toTime", value);
+                }}
+                style={styles.datePicker}
+              />
+            )}
+          </View>
+        </View>
+      </View>
+    );
   };
-  const renderLabelLocation = () => {
-    if (valueLocation || isFocus) {
-      return (
-        <Text style={[styles.label, isFocus && { color: Theme.colors.PRIMARY }]}>
-          Localidad
-        </Text>
-      );
-    }
-    return null;
+
+
+  const onPhotoUploaded = (fileKey) => {
+    setIsLoading(false);
+    setPictures([...pictures, fileKey]);
+    formik.setFieldValue("imageRest", pictures);
   };
-  const renderLabelNeighborhood = () => {
-    if (valueNeighborhood || isFocus) {
-      return (
-        <Text style={[styles.label, isFocus && { color: Theme.colors.PRIMARY }]}>
-          Barrio
-        </Text>
-      );
-    }
-    return null;
-  };
+
+  const onPhotoStartUpload = () => {
+    setIsLoading(true);
+  }
+
+  const onPhotoError = () => {
+    setIsLoading(false);
+    Toast("Hubo un error cargando la imagen")
+  }
+
+  const onOpenCloseMap = () => setShowMap((prevState)=>!prevState);
+
+  const removeImagen = (img)=> {
+    Alert.alert(
+      "Eliminar imagen",
+      "Estas seguro de eliminar esta imagen?",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel",  
+        },
+        {
+          text: "Eliminar",
+          onPress: () => {
+            const result = filter(formik.values.imageRest, (image)=> image !== img);
+            formik.setFieldValue("imageRest",result);
+            setPictures(result);
+          }
+        }
+      ]
+    );
+  }
 
   return (
+    <>
     <View style={styles.container1}>
-        <Button style={styles.circle} onPress={navigateToMenuOwner} title="<"/>
-          <Text style={styles.title}>Editar Restaurante</Text>
-          <Text style={styles.subTitle}>Datos principales</Text>
-          <View style={{alignItems: "center"}}>
-            <TextInput
-            style={styles.input}
-            placeholder='Nombre'
-            onChange={console.log("name")}
-            placeholderTextColor={Theme.colors.PRIMARY}
+      <Button style={styles.circle} onPress={null} title="<" />
+      <Text style={styles.title}>Editar Restaurante</Text>
+      <Text style={styles.subTitle}>Datos principales</Text>
+      <View style={{ alignItems: "center"}}>
+          <Input
+          placeholder='Nombre'
+          value={name}
+          onChangeText={(text) => {setName(text); 
+          formik.setFieldValue("name", name)}}
+          errorMessage={formik.errors.name}
+          />
+        <Input
+          rightIcon={{
+            type: "material-community",
+            name: "map-marker-radius-outline",
+            color: getColorIconMap(formik),
+            onPress: onOpenCloseMap
+          }}
+          value={street}
+          placeholder='Calle'
+          onChangeText={(text) => setStreet(text) }
+          errorMessage={formik.errors.street}
+          />
+          <Input
+          placeholder='Número'
+          onChangeText={(text) => setNumber (text) }
+          errorMessage={formik.errors.number}
+          value={number}
+          />
+        <FileUploadButton 
+          title={"+ Agregar Fotos"}
+          onSuccess={onPhotoUploaded}
+          onStartUpload={onPhotoStartUpload}
+          onError={onPhotoError} 
+        />
+        <ScrollView  horizontal showsHorizontalScrollIndicator={false}>
+        {pictures.map(p => {
+            return (
+              <Avatar
+                key={p}
+                uri={p}
+                styles={styles.imageStyle}
+                onPress={()=>removeImagen(p)}
+              />
+            )
+          })}
+        <Text style={styles.error}>
+          {formik.errors.imageRest}
+        </Text>
+        </ScrollView>
+        <LoadingModal show={isLoading} text='Subiendo imagen'/>
+        <Text style={styles.subTitle}>Días de apertura</Text>
+        </View>
+      {businessHours.map((businessHour,i) => {
+        return renderBusinessHour(businessHour, i);
+      })}
+      <View style={styles.container}>
+        <Dropdown
+          style={styles.dropdown}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={dataTypeFood}
+          labelField="label"
+          valueField="value"
+          placeholder="Seleccionar especialidad"
+          value={selected}
+          search
+          searchPlaceholder="Buscar..."
+          onChange={item => {
+            setSelected(item);
+          }}
+          renderLeftIcon={() => (
+            <AntDesign
+              style={styles.icon}
+              color="black"
+              name="Safety"
+              size={20}
             />
-            <TextInput
-            style={styles.input}
-            placeholder='Calle'
-            onChange={console.log("street")}
-            placeholderTextColor={Theme.colors.PRIMARY}
-            />
-    <View style={styles.container2}>
-      {renderLabelCountry()}
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: Theme.colors.PRIMARY }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={dataCountry}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'País' : '...'}
-        searchPlaceholder="Buscar..."
-        value={valueCoutry}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-          setValueCoutry(item.value);
-          setIsFocus(false);
+          )}
+          renderItem={renderItem}
+          renderSelectedItem={(item, unSelect) => (
+            <TouchableOpacity onPress={() => unSelect && unSelect(item)}>
+              <View style={styles.selectedStyle}>
+                <Text style={styles.textSelectedStyle}>{item.label}</Text>
+                <AntDesign color="black" name="delete" size={17} />
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+      <View style={styles.container2}>
+        {renderLabelPrice()}
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: Theme.colors.PRIMARY }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={dataPrice}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? 'Rango de precio' : '...'}
+          searchPlaceholder="Buscar..."
+          value={valuePriece}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={item => {
+            setValuePriece(item.value);
+            setIsFocus(false);
+          }}
+        />
+      </View>
+      
+      <ListItem
+        title="Cerrar Temporalmente"
+        trailing={
+          <Switch value={checked} onValueChange={() => setChecked(!checked)} />
+        }
+        onPress={() => {
+          setChecked(!checked)
         }}
       />
+      <Text style={styles.closeSubTitle}>Si activas esta opción tu restaurante comenzará en modo inactivo</Text>
+        <Button style={styles.button2} onPress={onRestaurantSubmit} title="Continuar >" color={Theme.colors.PRIMARY} />
     </View>
-    <View style={styles.container2}>
-      {renderLabelProvince()}
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: Theme.colors.PRIMARY }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={dataProvince}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'Provincia' : '...'}
-        searchPlaceholder="Buscar..."
-        value={valueProvince}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-          setValueProvince(item.value);
-          setIsFocus(false);
-        }}
-      />
-    </View>
-    <View style={styles.container2}>
-      {renderLabelLocation()}
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: Theme.colors.PRIMARY }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={dataLocation}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'Localidad' : '...'}
-        searchPlaceholder="Buscar..."
-        value={valueLocation}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-          setValueLocation(item.value);
-          setIsFocus(false);
-        }}
-      />
-    </View>
-    <View style={styles.container2}>
-      {renderLabelNeighborhood()}
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: Theme.colors.PRIMARY }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={dataNeighborhood}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'Barrio' : '...'}
-        searchPlaceholder="Buscar..."
-        value={valueNeighborhood}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-          setValueNeighborhood(item.value);
-          setIsFocus(false);
-        }}
-      />
-    </View>
-    <TextInput
-            style={styles.input}
-            placeholder='Número'
-            onChange={console.log("number")}
-            placeholderTextColor={Theme.colors.PRIMARY}
-            keyboardType='numeric'
-            />
-            <Button style={styles.button1} onPress={() => loginHandler()} title="+ Agregar Fotos" color={Theme.colors.SECONDARY}/>
-             <Button style={styles.button2} onPress={navigateToEditScreen2} title="Continuar >" color={Theme.colors.PRIMARY}/>
-          </View> 
-       </View>
+    <MapForm show={showMap} close={onOpenCloseMap} formik={formik}/>
+    </>
   );
 };
 
 export default EditRestaurantScreen1UI;
+
+const getColorIconMap = (formik) =>{
+  if(formik.errors.location) return Theme.colors.ERROR;
+  if(formik.values.location) return Theme.colors.PRIMARY;
+  return 'c2c2c2';
+}
 
 const styles = StyleSheet.create({
   container1:{
@@ -317,5 +426,124 @@ const styles = StyleSheet.create({
     width: 150,
     margin: 13,
     alignSelf: "flex-end",
+  },
+  MainContainer: {
+    flex: 1,
+    //padding: 6,
+    alignItems: 'center',
+    backgroundColor: Theme.colors.GREY,
+    flexDirection: "row", justifyContent: "space-between"
+  },
+
+  text: {
+    fontSize: 14,
+    color: Theme.colors.PRIMARY,
+  },
+  container: {
+    padding: 16
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: Theme.colors.GREY,
+    zIndex: 999,
+    left: 22,
+    top: 8,
+    paddingHorizontal: 8,
+    fontSize: 14,
+    color: Theme.colors.PRIMARY,
+  },
+  textSelectedStyle: {
+    marginRight: 5,
+    fontSize: 16,
+  },
+  selectedStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 14,
+    backgroundColor: Theme.colors.SECONDARY,
+    shadowColor: '#000',
+    marginTop: 8,
+    marginRight: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  food: {
+    width: 250,
+    margin: 13,
+    alignSelf: "center",
+  },
+  closeSubTitle: {
+    fontSize: 15,
+    color: "gray",
+    marginLeft: 10,
+    marginBottom: 10,
+    color: Theme.colors.SECONDARY,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#FE4164",
+  },
+  buttonClose: {
+    backgroundColor: Theme.colors.PRIMARY,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  days: {
+    fontSize: 20,
+    color: Theme.colors.PRIMARY,
+  },
+  buttonD: {
+    marginRight: 1,
+    color: Theme.colors.SECONDARY,
   },
 });
